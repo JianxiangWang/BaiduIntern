30a32f2d93574be081846c4ace072c97cb3e809a
JianxiangWang <w51141201062@163.com>
2016-07-21 19:47:21 +0800

    origin

--- zh_deepdive/test.py
+++ zh_deepdive/test.py
@@ -11,8 +11,28 @@ sys.setdefaultencoding('utf-8')
 
 
 
-s = [1, 3,  5]
+def _get_begin_index_and_end_index(tokens, char_start_index, char_length):
 
-t = set([1, 4])
+    char_end_index = char_start_index + char_length - 1
 
-print t.intersection(s)
+    begin_index = None
+    end_index = None
+
+    offset = 0
+    for index, token in enumerate(tokens) :
+        curr_token_char_range = list(range(offset, offset + len(unicode(token))))
+
+        if char_start_index in curr_token_char_range:
+            begin_index = index
+
+        if char_end_index in curr_token_char_range:
+            end_index = index
+
+        if begin_index and end_index:
+            break
+
+        offset += len(unicode(token))
+
+    return begin_index, end_index
+
+tokens = ["我是", "中国", "呃我", "饿", "23"]

f7bbb73250052864c26d2e3d357c5deaf5868c06
JianxiangWang <w51141201062@163.com>
2016-07-26 16:05:30 +0800

    origin

--- SPO_url/Ps/xiaoshuo/main.py
+++ SPO_url/Ps/xiaoshuo/main.py
@@ -1,5 +1,6 @@
 #encoding: utf-8
 import sys
+import os
 
 # 输入url, 判断是不是 小说
 def main():
@@ -8,3 +9,10 @@ def main():
 
 
 def is_xiaoShuo(url):
+
+    cmd = "../../tools/run_wdbtools-pc.sh %s" % (url)
+    print os.popen(cmd)
+
+
+if __name__ == '__main__':
+    is_xiaoShuo("http://www.2828dy.com/bbb/70728.html")

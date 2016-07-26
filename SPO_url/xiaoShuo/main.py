2fcf2b8344725fd9b18be841a9298ffa8771d87a
JianxiangWang <w51141201062@163.com>
2016-07-26 19:48:23 +0800

    origin

--- SPO_url/xiaoShuo/main.py
+++ SPO_url/xiaoShuo/main.py
@@ -3,26 +3,30 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import subprocess
-import sys
+import sys, hashlib
 import os
 
 # 输入url, 判断是不是 小说
-def main():
-    for line in sys.stdin:
+def main(urls_file, predict_file):
+
+    fout = open(predict_file, "w")
+    for line in open(urls_file):
         url = line.strip()
         if is_xiaoShuo(url):
-            S = "title"
+            title = get_url_title(url)
+            S = title
             P = "体裁/小说"
             O = url
 
-            print("%s\t%s\t%s" % (S, P, O))
+            fout.write("%s\t%s\t%s\n" % (S, P, O))
+
+    fout.close()
 
 
 def is_xiaoShuo(url):
 
     cmd = "../tools/run_wdbtools-pc.sh %s" % (url)
     # result = subprocess.check_output(cmd, shell=True)
-
     fin = os.popen(cmd)
     result = fin.readlines()[-1]
 
@@ -32,7 +36,49 @@ def is_xiaoShuo(url):
     else:
         return False
 
+def get_url_title(url):
+
+    pack_file_path = _get_pack_file_path(url)
+    # 根据pack,获取对应的title
+    title = get_title_by_pack_file(pack_file_path)
+
+    return title
+
+
+
+def _get_pack_file_path(url):
+    m = hashlib.md5()
+    m.update(url)
+    file_name = m.hexdigest()
+
+    pack_file_path = "packs/%s" % (file_name)
+    if os.path.exists(pack_file_path):
+        return pack_file_path
+    else:
+        # 抓url对应的pack
+        wdbtools_path = "/home/disk2/wangjianxiang01/tools/wdbtools/output/client/bin"
+        #  抓包 !
+        cwd = "%s/seekone %s PAGE > %s " % (wdbtools_path, url, pack_file_path)
+        os.system(cwd)
+        #  删除前2行
+        cwd = "sed '2d' %s > tmp.txt && mv tmp.txt %s" % (pack_file_path, pack_file_path)
+        os.system(cwd)
+        return pack_file_path
+
+
+def get_title_by_pack_file(pack_file):
+    # cat pack.test.input | /test_vareamark -t realtitle -o 0 | iconv -f gb18030 -t utf-8
+    varemark_path = "/home/disk2/wangjianxiang01/tools/varemark"
+
+    cmd = "cat %s | %s/test_vareamark -t realtitle -o 0 | iconv -f gb18030 -t utf-8" % (pack_file, varemark_path)
+    fin = os.popen(cmd)
+    result = fin.readlines()[-1]
+
+    title = result.split(" | ")[-1]
+
+    return title
+
 
 
 if __name__ == '__main__':
-    main()
+    main("urls.txt", "predict.txt")

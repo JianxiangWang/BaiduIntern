f4fc1d9b5c0e9239eb8af9faceeb3777c4ba37d8
JianxiangWang <w51141201062@163.com>
2016-07-28 11:47:48 +0800

    origin

--- SPO_url/tuPian/main.py
+++ SPO_url/tuPian/main.py
@@ -27,7 +27,7 @@ def main():
 
 def is_tupian(url):
     html_path = get_url_tagged_content_html_path(url)
-    soup = BeautifulSoup(open(html_path), "lxml")
+    soup = BeautifulSoup(open(html_path))
 
     #
     images = get_all_images(soup)

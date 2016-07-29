895be0283d4519cb0d556fd7e63d82d3a106faa6
JianxiangWang <w51141201062@163.com>
2016-07-29 11:19:23 +0800

    origin

--- zh_deepdive/load_deepdive_data.py
+++ zh_deepdive/load_deepdive_data.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 import sys
-
 import pyprind
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import codecs, json
@@ -26,6 +24,14 @@ def load_train_data(in_file, to_file, statistics_file):
 
             dict_label_info = json.loads(dict_label_info)
 
+            # 过滤一下SO识别
+            dict_so = json.loads(S_O)
+            dict_so_new = {}
+            for P in dict_so:
+                if P in dict_label_info:
+                    dict_so_new[P] = dict_so[P]
+
+
             # 加载每个P的正负样本:
             for P in dict_label_info:
 
@@ -41,12 +47,21 @@ def load_train_data(in_file, to_file, statistics_file):
                     #  正例
                     if label > 0:
                         dict_P_to_count[P]["positive"] += 1
-                        fout.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (sent_text, S_text, P, O_text, "1", line.strip()))
+
+                        fout.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (sent_text, S_text, P, O_text, "1",
+                                                                 json.dumps(dict_label_info, ensure_ascii=False),
+                                                                 json.dumps(dict_so_new, ensure_ascii=False),
+                                                                 )
+                        )
 
 
                     if label < 0 and dict_P_to_count[P]["negative"] < NUM_NEAGTIVE:
                         dict_P_to_count[P]["negative"] += 1
-                        fout.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (sent_text, S_text, P, O_text, "-1", line.strip()))
+                        fout.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (sent_text, S_text, P, O_text, "-1",
+                                                                 json.dumps(dict_label_info, ensure_ascii=False),
+                                                                 json.dumps(dict_so_new, ensure_ascii=False),
+                                                                 )
+                        )
 
     with open(statistics_file, "w") as fout:
         for P in dict_P_to_count:

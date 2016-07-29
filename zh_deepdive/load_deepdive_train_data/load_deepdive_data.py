ade8bbce6d223a1f27bd1f99af11e0adb8ef1b05
JianxiangWang <w51141201062@163.com>
2016-07-29 14:19:00 +0800

    origin

--- zh_deepdive/load_deepdive_train_data/load_deepdive_data.py
+++ zh_deepdive/load_deepdive_train_data/load_deepdive_data.py
@@ -91,6 +91,61 @@ def load_train_data(in_file, to_file, statistics_file):
             fout.write("%s\t%d\t%d\n" % (P, dict_P_to_count[P]["positive"], dict_P_to_count[P]["negative"]))
 
 
+def tagging_train_data(in_file, to_file, statistics_file):
+
+    dict_P_to_count = {}
+
+    NUM_NEAGTIVE = 100000
+
+    process_bar = pyprind.ProgPercent(1175207)
+    with open(in_file) as fin, \
+         open(to_file, "w") as fout:
+
+        for line in fin:
+            process_bar.update()
+
+            line_list = line.strip().split("\t")
+            sent_id, sent_text, tokens, pos_tags, ner_tags, dep_types, dep_tokens,\
+            S_O, dict_label_info_string = line_list
+
+            dict_label_info = ujson.loads(dict_label_info_string)
+
+
+
+            # 加载每个P的正负样本:
+            for P in dict_label_info:
+
+                if P not in dict_P_to_count:
+                    dict_P_to_count[P] = {}
+                    dict_P_to_count[P]["positive"] = 0
+                    dict_P_to_count[P]["negative"] = 0
+
+                for s_o in dict_label_info[P]["candidates"]:
+                    label = dict_label_info[P]["candidates"][s_o]["label"]
+
+                    #  正例
+                    if label > 0:
+                        dict_P_to_count[P]["positive"] += 1
+                        dict_label_info[P]["candidates"][s_o]["wanted"] = 1
+                    else:
+                        # 负例
+                        if label < 0 and dict_P_to_count[P]["negative"] < NUM_NEAGTIVE:
+                            dict_P_to_count[P]["negative"] += 1
+
+                            dict_label_info[P]["candidates"][s_o]["wanted"] = 1
+                        else:
+                            dict_label_info[P]["candidates"][s_o]["wanted"] = 0
+
+            out_line_list = line_list[:-1] + [ujson.dumps(dict_label_info, ensure_ascii=False)]
+            fout.write("\t".join(out_line_list) + "\n")
+
+
+
+    with open(statistics_file, "w") as fout:
+        for P in dict_P_to_count:
+            fout.write("%s\t%d\t%d\n" % (P, dict_P_to_count[P]["positive"], dict_P_to_count[P]["negative"]))
+
+
 def load_train_data_hadoop():
 
     dict_P_to_count = {}
@@ -146,9 +201,15 @@ def load_train_data_hadoop():
 
 if __name__ == '__main__':
     # load_train_data("/home/jianxiang/pycharmSpace/BaiduIntern/zh_deepdive/data/SPO_train_data_for_deepdive_label")
-    load_train_data(
+    # load_train_data(
+    #     "/home/disk2/wangjianxiang01/downloads/SPO_train_data_84P_for_deepdive_label_1w_pos_5w_neg",
+    #     "/home/disk2/wangjianxiang01/downloads/SPO_train_data_84P_for_deepdive_label_1w_pos_10w.spo.sent",
+    #     "/home/disk2/wangjianxiang01/downloads/SPO_train_data_84P_for_deepdive_label_1w_pos_10w.spo.sent.statistics",
+    # )
+
+    tagging_train_data(
         "/home/disk2/wangjianxiang01/downloads/SPO_train_data_84P_for_deepdive_label_1w_pos_5w_neg",
-        "/home/disk2/wangjianxiang01/downloads/SPO_train_data_84P_for_deepdive_label_1w_pos_10w.spo.sent",
+        "/home/disk2/wangjianxiang01/downloads/SPO_train_data_84P_for_deepdive_label_1w_pos_5w_neg.tagged",
         "/home/disk2/wangjianxiang01/downloads/SPO_train_data_84P_for_deepdive_label_1w_pos_10w.spo.sent.statistics",
     )
 

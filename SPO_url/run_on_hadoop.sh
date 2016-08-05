3caf2314e945bc076bbb54d73610ef426147533f
JianxiangWang <w5114120162@163.com>
2016-08-05 11:59:59 +0800

    wd

--- SPO_url/run_on_hadoop.sh
+++ SPO_url/run_on_hadoop.sh
@@ -3,7 +3,7 @@
 
 [ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
 
-cat $input | ba/hadoop_main.py
+#cat $input | ba/hadoop_main.py
 cat $input | shipin/hadoop_main.py
 cat $input | tuPian/hadoop_main.py
 cat $input | xiaoShuo/hadoop_main.py

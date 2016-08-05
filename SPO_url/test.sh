7f68c960e82b16b9ff9f4893144a5207caa32e0b
JianxiangWang <w51141201062@163.com>
2016-08-05 17:24:27 +0800

    Merge branch 'master' of github.com:JianxiangWang/BaiduIntern
    # Conflicts:
    #	SPO_url/run_on_hadoop.py

--- SPO_url/test.sh
+++ SPO_url/test.sh
@@ -11,4 +11,4 @@ cat pipe2 | (tuPian/hadoop_main.py) &
 cat pipe3 | (xiaoShuo/hadoop_main.py) &
 cat pipe4 | (xiazai/hadoop_main.py) &
 
-cat $input | tee pipe0 pipe1 pipe2 pipe3 pipe4 pipe5 | yinpin/hadoop_main.py
+cat $input | tee pipe0 pipe1 pipe2 pipe3 pipe4 | yinpin/hadoop_main.py

8106b65b76188074ab791a9fdf5cadbf5c6d75b7
JianxiangWang <w51141201062@163.com>
2016-08-05 12:15:37 +0800

    origin

--- SPO_url/run_on_hadoop.sh
+++ SPO_url/run_on_hadoop.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env bash
 
 
-#[ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
-#
-#cat $input | ba/hadoop_main.py
-#cat $input | shipin/hadoop_main.py
-#cat $input | tuPian/hadoop_main.py
-#cat $input | xiaoShuo/hadoop_main.py
-#cat $input | xiazai/hadoop_main.py
-#cat $input | yinpin/hadoop_main.py
-
-while read line
+while read -r line
 do
 #    echo ${line}
     # 吧
     echo -E "${line}" | ba/hadoop_main.py
-#    # 视频
-#    echo ${line} | shipin/hadoop_main.py
-#    # 图片
-#    echo ${line} | tuPian/hadoop_main.py
-#    # 小说
-#    echo ${line} | xiaoShuo/hadoop_main.py
-#    # 下载
-#    echo ${line} | xiazai/hadoop_main.py
-#    # 音频
-#    echo ${line} | yinpin/hadoop_main.py
+    # 视频
+    echo -E ${line} | shipin/hadoop_main.py
+    # 图片
+    echo -E ${line} | tuPian/hadoop_main.py
+    # 小说
+    echo -E ${line} | xiaoShuo/hadoop_main.py
+    # 下载
+    echo -E ${line} | xiazai/hadoop_main.py
+    # 音频
+    echo -E ${line} | yinpin/hadoop_main.py
 ##    # 介峰部分
 ##    cd
 ##        ...

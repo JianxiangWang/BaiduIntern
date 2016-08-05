1877c11a16fa9a2ca83d42329d0657259c462f50
JianxiangWang <w5114120162@163.com>
2016-08-05 11:50:31 +0800

    as

--- SPO_url/run_on_hadoop.py
+++ SPO_url/run_on_hadoop.py
@@ -2,6 +2,6 @@ import sys, os
 
 for line in sys.stdin:
 
-    cmd = "echo '%s' | ba/hadoop_main.py" % line
+    cmd = "echo -ne \"%s\" | ba/hadoop_main.py" % line
 
     os.system(cmd)
--- SPO_url/run_on_hadoop.sh
+++ SPO_url/run_on_hadoop.sh
@@ -3,22 +3,7 @@
 
 while read line
 do
-    echo ${line}
     # 吧
-    echo ${line} | ba/hadoop_main.py
-    # 视频
-    echo ${line} | shipin/hadoop_main.py
-    # 图片
-    echo ${line} | tuPian/hadoop_main.py
-    # 小说
-    echo ${line} | xiaoShuo/hadoop_main.py
-    # 下载
-    echo ${line} | xiazai/hadoop_main.py
-    # 音频
-    echo ${line} | yinpin/hadoop_main.py
-#    # 介峰部分
-#    cd
-#        ...
-#    cd
+    cat ${line} | ba/hadoop_main.py
 
 done

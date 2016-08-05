c50722a749dea581b4d484104be0a5eaf6041d2e
JianxiangWang <w51141201062@163.com>
2016-08-05 11:50:00 +0800

    origin

--- SPO_url/run_on_hadoop.sh
+++ SPO_url/run_on_hadoop.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
 
-while read line
-do
-    echo ${line}
-    # 吧
-    echo ${line} | ba/hadoop_main.py
-    # 视频
-    echo ${line} | shipin/hadoop_main.py
-    # 图片
-    echo ${line} | tuPian/hadoop_main.py
-    # 小说
-    echo ${line} | xiaoShuo/hadoop_main.py
-    # 下载
-    echo ${line} | xiazai/hadoop_main.py
-    # 音频
-    echo ${line} | yinpin/hadoop_main.py
-#    # 介峰部分
-#    cd
-#        ...
-#    cd
+[ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
 
-done
+cat $input | ba/hadoop_main.py
+
+#while read line
+#do
+#    echo ${line}
+#    # 吧
+#    echo ${line} | ba/hadoop_main.py
+#    # 视频
+#    echo ${line} | shipin/hadoop_main.py
+#    # 图片
+#    echo ${line} | tuPian/hadoop_main.py
+#    # 小说
+#    echo ${line} | xiaoShuo/hadoop_main.py
+#    # 下载
+#    echo ${line} | xiazai/hadoop_main.py
+#    # 音频
+#    echo ${line} | yinpin/hadoop_main.py
+##    # 介峰部分
+##    cd
+##        ...
+##    cd
+#
+#done

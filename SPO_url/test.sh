4322481390af608656aef49fa532ee568cbf3fce
JianxiangWang <w51141201062@163.com>
2016-08-05 17:22:01 +0800

    Merge branch 'master' of github.com:JianxiangWang/BaiduIntern
    # Conflicts:
    #	SPO_url/run_on_hadoop.py

--- SPO_url/test.sh
+++ SPO_url/test.sh
@@ -3,7 +3,7 @@
 [ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
 #cat $input | tee >(ba/hadoop_main.py)  | tuPian/hadoop_main.py
 
-mkfifo pipe pipe1
+mkfifo pipe pipe1 pipe2 pipe3 pipe4 pipe5
 
 cat pipe0 | (ba/hadoop_main.py) &
 cat pipe1 | (shipin/hadoop_main.py) &

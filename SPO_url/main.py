b6ec862f582836fddad195754959dc4cbd225dbd
JianxiangWang <w51141201062@163.com>
2016-08-05 18:41:54 +0800

    yes !

--- SPO_url/main.py
+++ SPO_url/main.py
@@ -4,13 +4,13 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import json
-from jiefeng.PageClassify import PageClassify
 from ba.hadoop_main import do_extraction as ba_extraction
 from shipin.hadoop_main import do_extraction as shipin_extraction
 from tuPian.hadoop_main import do_extraction as tuPian_extraction
 from xiaoShuo.hadoop_main import do_extraction as xiaoShuo_extraction
 from xiazai.hadoop_main import do_extraction as xiazai_extraction
 from yinpin.hadoop_main import do_extraction as yinpin_extraction
+from jiefeng.PageClassify import PageClassify
 
 # mine
 extractions = [

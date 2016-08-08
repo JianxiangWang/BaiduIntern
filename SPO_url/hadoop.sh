6c86535e73bee1e677c12b19e10ba162b1455b0c
JianxiangWang <w51141201062@163.com>
2016-08-08 18:04:43 +0800

    yes !

--- SPO_url/hadoop.sh
+++ SPO_url/hadoop.sh
@@ -23,10 +23,10 @@ hadoop streaming \
     -jobconf stream.num.map.output.key.fields=4 \
     -jobconf mapred.output.compress=true \
     -jobconf mapred.compress.map.output=true \
-    -jobconf mapred.map.tasks=5000 \
-    -jobconf mapred.job.map.capacity=400 \
+    -jobconf mapred.map.tasks=8000 \
+    -jobconf mapred.job.map.capacity=300 \
     -jobconf mapred.reduce.tasks=0 \
-    -jobconf mapred.job.reduce.capacity=400 \
+    -jobconf mapred.job.reduce.capacity=300 \
     -jobconf mapred.job.name="wangjianxiang_qdh"
 
 #    -file "main.py" \

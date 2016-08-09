1cb038376d2c9a366478b8e7c0468d4daa2cfecc
JianxiangWang <w51141201062@163.com>
2016-08-09 13:15:39 +0800

    yes !

---
+++ SPO_url/coverage/2_get_spo_title/get_s_set.py
@@ -0,0 +1,27 @@
+#  encoding: utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def main(fin):
+
+    s_set = set()
+
+    for line in fin:
+        line_list = line.strip().split("\t")
+        if len(line_list) == 1:
+            continue
+
+        # 获取第一个NER识别结果
+        r = line_list[1][1:-1].strip()
+        for x in r.split("  "):
+            k, v = x.split(" : ")
+            if k == "name":
+                s_set.add(v)
+                break
+
+    for s in sorted(s_set):
+        print s
+
+if __name__ == '__main__':
+    main(sys.stdin)

648c99dc6aac0ca781087a9e5667c72ccc123352
JianxiangWang <w51141201062@163.com>
2016-08-11 15:20:10 +0800

    yes !

--- SPO_url/test_data/org/main.py
+++ SPO_url/test_data/org/main.py
@@ -44,4 +44,5 @@ if __name__ == '__main__':
     # main("评测", "评测", "pingce.test.data")
     # main("简介", "简介", "jianjie.test.data")
     # main("个人资料", "个人资料", "gerenziliao.test.data")
-    main("百科", "百科", "baike.test.data")
+    # main("百科", "百科", "baike.test.data")
+    main("微博", "微博", "weibo.test.data")

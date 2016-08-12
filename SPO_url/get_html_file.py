2a54ded925d266fbfc8bb3574a46dfd8641a3c06
JianxiangWang <w51141201062@163.com>
2016-08-12 16:26:15 +0800

    yes !

---
+++ SPO_url/get_html_file.py
@@ -0,0 +1,21 @@
+import sys
+
+import json
+
+
+def main(parse_file, url):
+
+    for line in open(parse_file):
+        line_list = line.strip().split("\t")
+        this_url = line_list[0].strip()
+        dict_info_str = line_list[-1].strip()
+
+        if this_url.strip() == url:
+            dict_info = json.loads(dict_info_str)
+            print dict_info["cont_html"]
+
+
+if __name__ == '__main__':
+    parse_file = sys.argv[1]
+    url = sys.argv[2]
+    main(parse_file, url)

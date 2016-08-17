cfd42960d8d3bc925d492a7bd4e05fb51102c51f
JianxiangWang <w51141201062@163.com>
2016-08-17 15:18:26 +0800

    yes !

--- SPO_url/xiaoShuo/hadoop_main.py
+++ SPO_url/xiaoShuo/hadoop_main.py
@@ -29,8 +29,7 @@ def do_extraction(url, dict_info, soup):
     x, confidence = is_xiaoShuo(url, dict_info, soup)
     if x:
         url = unicode(url, errors="ignore")
-        title = dict_info["realtitle"]
-        S = title
+        S = get_s(url, dict_info, soup)
         P = u"小说"
         O = url
 
@@ -44,6 +43,55 @@ def is_xiaoShuo(url, dict_info, soup):
     else:
         return (False, 0)
 
+
+def get_s(url, dict_info, soup):
+    # 1. 如果title中有《》,将《》里面的内容作为s
+    title  = dict_info["realtitle"]
+    if u"《" in title and u"》" in title:
+        s = title.find(u"《")
+        e = title.find(u"》")
+        if s < e:
+            return remove_flanking_symbols(title[s+1: e])
+
+    # 2. 删除一些无用的词
+    useless_words = [
+        u"最新章节列表",
+        u"简介",
+        u"txt",
+        u"全文免费阅读",
+        u"最新章节",
+        u"更新列表",
+        u"TXT",
+        u"全集",
+        u"下载"
+    ]
+    for word in useless_words:
+        title = title.replace(word, "")
+
+    return remove_flanking_symbols(title)
+
+
+def remove_flanking_symbols(str):
+
+    en_punctuations = u""" !"#&'*+,-..../:;<=>?@[\]^_`|~%""" + "``" + "''"
+    ch_punctuations = u"``''，。；、：？！∶… …──“”＊「」《》【】"
+    nums = u"0123456789"
+    symbols = en_punctuations + ch_punctuations + nums
+    return _remove_flanking_symbols(str, symbols)
+
+
+
+def _remove_flanking_symbols(string, symbols):
+    i = 0
+    while i < len(string) and (string[i] in symbols):
+        i += 1
+    if i == len(string):
+        return ""
+
+    j = len(string) - 1
+    while j >= 0 and (string[j] in symbols):
+        j -= 1
+    return string[i: j + 1]
 
 if __name__ == '__main__':
     main()

a2cd5cf638aa158a3c742c363dea0a45209eb482
JianxiangWang <w51141201062@163.com>
2016-08-21 20:01:18 +0800

    yes !

--- SPO_url/s_identification/get_some_predict.py
+++ SPO_url/s_identification/get_some_predict.py
@@ -23,14 +23,31 @@ def get_someP_title(in_file, to_file):
             fout.write(s + "\n")
 
 
+# 将预测结果的ner识别,放到预测结果中去
+def add_ner_info(predict_file_path, predict_ner_file_path, to_file):
+
+    with open(predict_file_path) as predict_file, open(predict_ner_file_path) as predict_ner_file,\
+         open(to_file, "w") as fout:
+
+        for line1, line2 in zip(predict_file, predict_ner_file):
+            new_line = line1.strip() + "\t" + line2.strip().split("\t")[-1] + "\n"
+            fout.write(new_line)
+
+
+
 if __name__ == '__main__':
     # main(
     #     "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo",
     #     "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP"
     # )
 
-    get_someP_title(
-        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP",
-        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP.title"
-    )
+    # get_someP_title(
+    #     "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP",
+    #     "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP.title"
+    # )
 
+    add_ner_info(
+        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP",
+        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP.title.ner",
+        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP.ner",
+    )

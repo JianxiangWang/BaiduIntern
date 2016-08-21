0a4ac7ab04afeed7c6224a335150e3f03c6f5ae1
JianxiangWang <w51141201062@163.com>
2016-08-21 16:41:50 +0800

    yes !

--- SPO_url/ba/hadoop_main.py
+++ SPO_url/ba/hadoop_main.py
@@ -60,7 +60,8 @@ def get_s(url, dict_info, soup):
     # 找不到使用title
     s = dict_info["realtitle"]
 
-    if s.endswith("吧"):
+    if s.endswith(u"吧"):
+        print "===>>>>>>>>>>"
         s = s[:-1]
     return s
 

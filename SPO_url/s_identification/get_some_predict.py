0473a0fa973959000f24047e491eb04d63cf0a99
JianxiangWang <w51141201062@163.com>
2016-08-21 17:48:29 +0800

    yes !

--- SPO_url/s_identification/get_some_predict.py
+++ SPO_url/s_identification/get_some_predict.py
@@ -14,10 +14,23 @@ def main(predict_file, to_file):
             if p in ["视频", "评测", "简介", "个人资料"]:
                 fout.write(line)
 
+# 获取 "视频", "评测", "简介", "个人资料" 的预测结果中的title
+def get_someP_title(in_file, to_file):
+
+    with open(in_file) as fin, open(to_file, "w") as fout:
+        for line in fin:
+            url, s, p, o, _ = line.strip().split("\t")
+            fout.write(s + "\n")
+
 
 if __name__ == '__main__':
-    main(
-        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo",
-        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP"
+    # main(
+    #     "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo",
+    #     "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP"
+    # )
+
+    get_someP_title(
+        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP",
+        "/home/disk2/wangjianxiang01/BaiduIntern/SPO_url/test_data/org/org.test.data.filtered.spo.someP.title"
     )
 

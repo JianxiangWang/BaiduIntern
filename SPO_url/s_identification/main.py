6331ca973b41edd64bb063d0e61f08b19c74ef1a
JianxiangWang <w51141201062@163.com>
2016-08-22 12:07:29 +0800

    yes !

--- SPO_url/s_identification/main.py
+++ SPO_url/s_identification/main.py
@@ -72,28 +72,47 @@ def get_s_for_jianjie(line):
 
     line_list = line.strip().split("\t")
     ner_list = eval(line_list[-1])
-    title = line_list[1]
+    title = line_list[1].strip()
 
-    if ner_list == []:
-        return title
+    if ner_list != []:
+        key_word_list = [u"简介", u"介绍"]
+        before_idx_list = [title.find(key_word) + len(key_word) for key_word in key_word_list if key_word in title ]
+        if before_idx_list == []:
+            before_idx = len(title)
+        else:
+            before_idx = min(before_idx_list)
 
-    key_word_list = [u"简介", u"介绍"]
-    before_idx_list = [title.find(key_word) + len(key_word) for key_word in key_word_list if key_word in title ]
-    if before_idx_list == []:
-        before_idx = len(title)
-    else:
-        before_idx = min(before_idx_list)
+        entity_name = None
+        for ner in ner_list:
+            offset = ner["offset"]
+            etype = ner["etype"]
 
-    entity_name = None
-    for ner in ner_list:
-        offset = ner["offset"]
-        etype = ner["etype"]
+            if offset < before_idx:
+                entity_name = ner["name"]
 
-        if offset < before_idx:
-            entity_name = ner["name"]
+        if entity_name:
+            return entity_name
 
-    if entity_name:
-        return entity_name
+    # 如果title以()结尾去掉()
+    if title.endswith("）"):
+        if "（" in title:
+            title = title[:title.rfind("（")]
+    if title.endswith(")"):
+        if "(" in title:
+            title = title[:title.rfind("(")]
+
+    # 如果有 【南妹皇后】, 《斗破苍穹》 去中间的
+    if u"《" in title and u"》" in title:
+        s = title.find(u"《")
+        e = title.find(u"》")
+        if s < e:
+            return title[s+1: e]
+
+    if u"【" in title and u"】" in title:
+        s = title.find(u"【")
+        e = title.find(u"】")
+        if s < e:
+            return title[s+1: e]
 
     return title
 

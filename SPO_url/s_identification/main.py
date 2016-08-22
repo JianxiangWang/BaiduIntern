9ba751ed78b1c232e912be8315b5875e6b6ddf88
JianxiangWang <w51141201062@163.com>
2016-08-22 18:02:45 +0800

    yes !

--- SPO_url/s_identification/main.py
+++ SPO_url/s_identification/main.py
@@ -264,6 +264,7 @@ def get_s_for_xiazai(line):
             offset = ner["offset"]
             etype = ner["etype"]
 
+            print title, ner
             if offset < before_idx:
                 entity_name = ner["name"]
                 break
